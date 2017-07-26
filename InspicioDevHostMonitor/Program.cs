5c1bee82be68217d800cd8af98263803d81f19c2
Wilka Hudson <wilka.hudson@nonlinear.com>
2017-07-26 13:21:59 +0100

    Add project files.

---
+++ InspicioDevHostMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InspicioDevHostMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
